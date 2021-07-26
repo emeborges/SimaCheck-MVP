c104feb73ed1681a64864747d397770bc70b7b31
Marcelo Borges <81544795+emeborges@users.noreply.github.com>
2021-07-25 21:09:15 -0300

    25072021 - Repaginamento
    Repaginamento do Index e filter
    
    Inicio do Repage.

---
+++ app.js
@@ -0,0 +1,63 @@
+// Módulo de Slider da parte de Checks
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+// Scroll Suave Menu
+
+const menuItems = document.querySelectorAll('.left_menu a');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+
+} )
+
+function scrollToIdOnClick(event){
+    event.preventDefault()
+    const element = event.target;
+    const id = element.getAttribute('href')
+    const sessao = document.querySelector(id).offsetTop
+
+    window.scroll({
+        top: sessao - 80,
+        behavior: "smooth",
+    })
+}
+
+// Encaminha para outra pg
+
+function redirecionamentoFilter(){
+    window.location.href = "/filter.html"
+}
+
+// Scroll Suave Bottom
+
+function rolagemSuave(event){
+    event.preventDefault
+
+    const destino = document.querySelector('#comoFunciona').offsetTop
+
+    window.scroll({
+        top: destino - 80,
+        behavior: "smooth",
+    })
+}
