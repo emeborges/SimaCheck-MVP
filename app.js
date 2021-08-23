9b5f2f2db61bf279bdaaf6cebbcebbcff1c89045
Marcelo Borges <81544795+emeborges@users.noreply.github.com>
2021-08-23 16:43:51 -0300

    teste

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
-//Efeito de rolagem lenta Links Menu, home
-const menuItens = document.querySelectorAll('.left_menu a') // Click e direcionamento suave para o resto da tela.
+// Módulo de Slider da parte de Checks
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
-function scrollToIdOnClick(){
-    event.preventDefault(); // Tira o evento padrão do Click! 
-    const element = event.target; // Aqui, ele busca qual obj html é referente do evento
-    const id = element.getAttribute('href'); //Busca o atributo referente ao objeto html
-    const to = document.querySelector(id).offsetTop; //Ele esta localizando o ID que esta no href, e trazendo a Coordenada de tal.    
+// Scroll Suave Menu
+
+const menuItems = document.querySelectorAll('.left_menu a');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+
+} )
+
+function scrollToIdOnClick(event){
+    event.preventDefault()
+    const element = event.target;
+    const id = element.getAttribute('href')
+    const sessao = document.querySelector(id).offsetTop
 
     window.scroll({
-        top: to - 80,
+        top: sessao - 80,
         behavior: "smooth",
-    }); // Faz o Scroll no objeto Janela até a altura obtida pela Const anterior, sobre o efeito escolhido
+    })
 }
 
-menuItens.forEach(item =>{ 
-    item.addEventListener('click', scrollToIdOnClick) // Adicinando o evento de clic novamente
-})
+// Encaminha para outra pg
 
-//Efeito de rolagem lenta, botão Home One 
-
-function rolagemSuave (){
-    const referenciaTo = document.querySelector('#comoFunciona').offsetTop;
-    const rept = window.scroll({
-                    top: referenciaTo - 80,
-                    behavior: "smooth",
-                    });
-
-    buttonHomeComoFunc.addEventListener('click', rept)
+function redirecionamentoFilter(){
+    window.location.href = "/filter.html"
 }
 
-//Funcoes de Direcionamento Para Outras Pages
-function redirecionamentoFilter (){
-    window.location.href = 'filter.html';
-} // redirecionamento do index para a selecao
+// Scroll Suave Bottom
 
-function redirecionamentoRadar (){
-    const mundoselecionado = document.querySelector("#selectWord").value
-    const localMundo = document.querySelector(".mundo")
+function rolagemSuave(event){
+    event.preventDefault
 
-    if(mundoselecionado === ""){
-        alert("Selecione o Mundo desejado")
-    } else {
-        window.location.href = 'radar.html'
-    }
+    const destino = document.querySelector('#comoFunciona').offsetTop
+
+    window.scroll({
+        top: destino - 80,
+        behavior: "smooth",
+    })
 }
